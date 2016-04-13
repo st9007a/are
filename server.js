1bee636a2d8924399b0c56d840a33eace41c3ad2
st9007a <st9007a@gmail.com>
2016-04-13 11:19:11 +0800

    add hw this week(4/13)

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 var app = require('express')();
 var express = require('express');
 var http = require('http').Server(app);
+var bodyParser = require('body-parser');
 
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
+
+
+app.post('/hw', function(req, res){
+    console.log(req.body);
+    if(req.body.id == 'e24026551'){
+	    res.send('吳哲銘')
+	}
+	else{
+	    res.send('not found');
+	}
+});
+
 app.get('/', function(req, res){
     res.sendfile('public/index.html');
+});
+app.get('/hw', function(req, res){
+    res.sendfile('public/hw.html');
 });
 http.listen(8011);
